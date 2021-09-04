966b45e6a33cbb1adeac06a4e344a1bfd95c1d5a
Bohdan <bogdan.05.den@gmail.com>
2021-09-04 20:50:42 +0300

    file with site configs

---
+++ src/siteconfig.js
@@ -0,0 +1,53 @@
+const seo = {
+    title: 'Portfolio',
+    description: 'Description',
+    keywords: ["Portfolio", "asd"]
+}
+export const DOMAIN = 'localhost:3000';
+export const URL = `http://${DOMAIN}`;
+export const SITENAME = 'Portfolio';
+export const EMAIL = 'denisenko.des@gmail.com';
+export const CHARACTERISTICS = {
+    skills: ['User Experience', 'User Interface', 'Visual Design', 'Prototyping', 'Design System creator', 'Wireframing', 'Interaction Design', 'Apple Human Guidelines & Google Material Des.'],
+    tools: ['Figma', 'Photoshop', 'Marvel App, Invision', 'Zeplin, Avocode', 'Trello, Jira, Asana', 'Hubstaff, Toptracker']
+}
+
+export const DRIBBBLE = "https://dribbble.com/workshopdesigner";
+export const BEHANCE = "https://www.behance.net/den1senko";
+export const INSTAGRAM = "https://www.instagram.com/workshopdesigner";
+
+export const LINKS = {
+    design: DRIBBBLE,
+    haloLab: 'https://www.halo-lab.com/',
+    games: 'https://steamcommunity.com/profiles/76561198339627224/',
+    anime: 'https://yummyanime.club/users/id373634',
+    music: 'https://open.spotify.com/playlist/1RoFQ6onJSF2HpSI7DxYMM?si=16078575a61f45b7',
+
+    dribbble: DRIBBBLE,
+    behance: BEHANCE,
+    instagram: INSTAGRAM
+}
+
+export const social =  {
+    url: URL,
+    sitename: SITENAME,
+    title: seo.title,
+    description: seo.description,
+    image: {
+        url: `${URL}/social.jpg`,
+        height: 1200,
+        width: 600,
+        alt: seo.description
+    }
+}
+
+export default {
+    ...seo,
+    social,
+    DOMAIN,
+    URL,
+    SITENAME,
+    email: EMAIL,
+    characteristics: CHARACTERISTICS,
+    links: LINKS
+}
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from "vite";
 import { minifyHtml, injectHtml } from "vite-plugin-html";
 import path from "path";
+import siteconfig from "./src/siteconfig";
+
 
 export default defineConfig({
     plugins: [
         injectHtml({
             injectData: {
-                email: 'denisenko.des@gmail.com',
-                characteristics: {
-                    skills: ['User Experience', 'User Interface', 'Visual Design', 'Prototyping', 'Design System creator', 'Wireframing', 'Interaction Design', 'Apple Human Guidelines & Google Material Des.'],
-                    tools: ['Figma', 'Photoshop', 'Marvel App, Invision', 'Zeplin, Avocode', 'Trello, Jira, Asana', 'Hubstaff, Toptracker']
-                }
+                ...siteconfig,
             },
             injectOptions: {
                 root: path.join(__dirname, "partials"),
